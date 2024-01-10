74084cbad427b4ca2516c0791b5afbcea85d0e53
timothytfchan <59618579+timothytfchan@users.noreply.github.com>
2024-01-11 03:05:12 +0800

    New results_to_history_str() function

--- main.py
+++ main.py
@@ -19,28 +19,30 @@ openai.organization = os.getenv('OPENAI_ORGANIZATION')
 Function calling hierarchy:
 main() calls run_game() multiple times in parallel and initializes objects of class Policy
 run_game() calls run_round() and update_results()
-run_round() calls query_policy() from object of class Policy
+run_round() calls query_policy() and results_to_history_str()
+results_to_history_str() goes into query_policy()
 query_policy() calls get_response_openai()
 get_response_openai() uses get_prompts()
-get_prompts() requires matrix_to_str() and results_to_history_str()
+get_prompts() uses matrix_to_str()
 """
 
 class Policy:
-	def __init__(self, model, player_name, opponent_name, role, persona, strategic_reasoning=False, system_dict={}, show_past_reasoning=False):
+	def __init__(self, model, player_name, opponent_name, role, persona, strategic_reasoning=False, sys_prompt_substitutions={}, show_past_reasoning=True, show_intended_actions=True):
 		self.player_name = player_name 
 		self.opponent_name = opponent_name # for prompt construction
 		self.role = role # for determining whether the player acts on rows or columns of matrix
 		self.persona = persona # for selecting data from csv file
 		self.strategic_reasoning = strategic_reasoning # whether to include optional strategic reasoning
-		self.system_dict = system_dict # for inserting more expressions into sys prompt (values replace keys in curly braces within template str)
+		self.sys_prompt_substitutions = sys_prompt_substitutions # for inserting more expressions into sys prompt (values replace keys in curly braces within template str)
 		self.model = model
 		self.show_past_reasoning = show_past_reasoning
+		self.show_intended_actions = show_intended_actions
 		self.most_recent_action = None
 
 	def query_policy(self, history_str, message_round, labels_row, labels_column, end_prob, matrix, total_message_rounds, **kwargs):
 		output = get_response_openai(self.player_name, self.opponent_name, self.role, self.persona, end_prob, history_str, matrix, 
 										labels_row, labels_column, message_round, total_message_rounds, strategic_reasoning=self.strategic_reasoning,
-										system_dict = self.system_dict, model=self.model, persona=self.persona, **kwargs)
+										sys_prompt_substitutions = self.sys_prompt_substitutions, model=self.model, persona=self.persona, **kwargs)
 		self.most_recent_action = output['action']
 		return output
 
@@ -58,89 +60,6 @@ def get_strategic_reasoning_prompt(examples_to_include=('simultaneous_22')):
 			prompt += f'\n{text}'
 	return prompt
 
-def results_to_history_str(policy, names_tuple, results, current_round_info=None, messages=False):
-	history_str = ''
-	n_rounds = len(results['time-steps'])
-	player_a, player_b = names_tuple[0], names_tuple[1]
-	show_past_reasoning = policy.show_past_reasoning
-	current_player = policy.player_name
-	
-	if messages:
-		# Summaries of previous rounds
-		for i in range(n_rounds):
-			messages_rounds = results['time-steps'][i]['messages']
-			for j, message in enumerate(messages_rounds):
-				summary_of_round = f"\nTime-step {i+1}.{j+1}"
-				speaker = message['speaker']
-				summary_of_round += f'\nSpeaker: {speaker}'
-
-				if (speaker == current_player) and show_past_reasoning:
-					# j//2 = position in speaker_output that the reasoning could be found
-					reasoning_content = results['time-steps'][i][f'{speaker}_output'][j//2]['reasoning']
-					summary_of_round += f'\nReasoning: {reasoning_content}'
-				
-				message_content = message['message']
-				summary_of_round += f'\nMessage content: {message_content}'
-			# Input final actions of round
-			player_a_action = results['time-steps'][i][f'{player_a}_output'][-1]['action']
-			player_b_action = results['time-steps'][i][f'{player_b}_output'][-1]['action']
-			summary_of_round += f'\nActions\n{player_a}: {player_a_action}, {player_b}: {player_b_action}'
-
-			history_str += summary_of_round
-
-			# This round's messages
-			#TODO: include current round reasoning if any
-			history_str += f'\nCurrent time-step messages'
-			if current_round_info:
-				for message in current_round_info:
-					speaker = message['speaker']
-					message_content = message['message']
-					history_str += f'\n{speaker}: {message_content}'
-			else:
-				history_str += '\nThis is the first message round for this time-step.'
-	else:
-		#TODO: include current round reasoning if any
-		for i in range(n_rounds):
-			summary_of_round = f'\nTime-step {i+1}'
-			
-			player_a_action = results['time-steps'][i][f'{player_a}_output']['action']
-			player_b_action = results['time-steps'][i][f'{player_b}_output']['action']
-			history_str += f"Actions\n{player_a_action},{player_b_action}"
-
-	if messages:
-		# Summaries of previous rounds
-		for i in range(n_rounds):
-			summary_of_round = f'\nTime-step {i+1}\nMessages'
-			messages_round = results['time-steps'][i]['messages']
-
-			for message in messages_round:
-				speaker = message['speaker']
-				message_content = message['message']
-				summary_of_round += f'\n{speaker}: {message_content}'
-			player_a_action = results['time-steps'][i][f'{player_a}_output'][-1]['action']
-			player_b_action = results['time-steps'][i][f'{player_b}_output'][-1]['action']
-			summary_of_round += f'\nActions\n{player_a_action},{player_b_action}'
-			history_str += summary_of_round
-		# This round's messages
-		history_str += f'\nCurrent time-step messages'
-		if current_round_info:
-			for message in current_round_info:
-				speaker = message['speaker']
-				message_content = message['message']
-				history_str += f'\n{speaker}: {message_content}'
-		else:
-			history_str += '\nThis is the first message round for this time-step.'
-	else:
-		for i in range(n_rounds):
-			summary_of_round = f'\nTime-step {i+1}'
-			
-			player_a_action = results['time-steps'][i][f'{player_a}_output']['action']
-			player_b_action = results['time-steps'][i][f'{player_b}_output']['action']
-			history_str += f"Actions\n{player_a_action},{player_b_action}"
-	
-	
-	
-	return history_str
 
 def matrix_to_str(matrix, labels_row, labels_column, player_name, opponent_name, role):
 	if role == 'row':
@@ -163,10 +82,10 @@ def matrix_to_str(matrix, labels_row, labels_column, player_name, opponent_name,
 	return matrix_str
 
 # Constructs prompts from the CSV and TXT files
-def get_prompts(player_name, opponent_name, role, persona, end_prob, history_str, matrix, labels_row, labels_column, message_round, total_message_rounds, strategic_reasoning, system_dict = {}):
+def get_prompts(player_name, opponent_name, role, persona, end_prob, history_str, matrix, labels_row, labels_column, message_round, total_message_rounds, strategic_reasoning, sys_prompt_substitutions = {}):
     # Construct system prompt
 	if strategic_reasoning:
-		system_dict["strategic_reasoning_prompt"] = get_strategic_reasoning_prompt()
+		sys_prompt_substitutions["strategic_reasoning_prompt"] = get_strategic_reasoning_prompt()
 	
 	if role == "row":
 		labels = labels_row
@@ -189,7 +108,7 @@ def get_prompts(player_name, opponent_name, role, persona, end_prob, history_str
 	system += f" {add_context} " if add_context else ""
 	system += f"You should return a JSON with the following format (key names should be in double quotes; you must always fill out every field):\n\n{output_schema}"
 	# Include any values that have not been added
-	system = system.format_map(defaultdict(str, **system_dict))
+	system = system.format_map(defaultdict(str, **sys_prompt_substitutions))
 
 	# Construct user prompt
 	if message_round > total_message_rounds:
@@ -202,8 +121,8 @@ def get_prompts(player_name, opponent_name, role, persona, end_prob, history_str
 	return system, user
 
 # Sends request and gets JSON response from the OpenAI API. In the future, we may want to add get_response_anthropic etc.
-def get_response_openai(player_name, opponent_name, role, persona, end_prob, history_str, matrix, labels_row, labels_column, message_round=None, total_message_rounds=None, strategic_reasoning=False, system_dict={}, model='gpt-4-32k', **kwargs):
-	system, user = get_prompts(player_name, opponent_name, role, persona, end_prob, history_str, matrix, labels_row, labels_column, message_round, total_message_rounds, strategic_reasoning=strategic_reasoning, system_dict=system_dict)
+def get_response_openai(player_name, opponent_name, role, persona, end_prob, history_str, matrix, labels_row, labels_column, message_round=None, total_message_rounds=None, strategic_reasoning=False, sys_prompt_substitutions={}, model='gpt-4-32k', **kwargs):
+	system, user = get_prompts(player_name, opponent_name, role, persona, end_prob, history_str, matrix, labels_row, labels_column, message_round, total_message_rounds, strategic_reasoning=strategic_reasoning, sys_prompt_substitutions=sys_prompt_substitutions)
 	max_retries = 5
 	retry_interval_sec = 20
 	answer_dict = {}
@@ -237,40 +156,98 @@ def get_response_openai(player_name, opponent_name, role, persona, end_prob, his
  
 	return answer_dict
 
+def results_to_history_str(policy, names_tuple, results, round_outputs=None, messages=False):
+	history_str = ''
+	player_a, player_b = names_tuple[0], names_tuple[1]
+	show_past_reasoning = policy.show_past_reasoning
+	show_intended_actions = policy.show_intended_actions
+	current_player = policy.player_name
+	# Summarize previous rounds using results
+	for i, round_ in results['rounds']:
+		history_str += f'Round {i+1}:\n'
+		# Summarize any combination of {reasoning, messages, actions}
+		for j, message_round in round_['message_rounds']:
+			if show_past_reasoning and (message_round[j]['player'] == current_player):
+				history_str += f"Your reasoning: {message_round[j]['reasoning']}\n"
+			
+			if messages and (message_round[j]['player'] == current_player):
+				history_str += f"Your message: {message_round[j]['message']}\n"
+
+			if messages and (message_round[j]['player'] != current_player):
+				history_str += f"Other party's message: {message_round[j]['message']}\n"
+
+			if show_intended_actions and (message_round[j]['player'] == current_player):
+				history_str += f"Your intended action at t={j+1} within this round: {message_round[j]['action']}\n"
+
+			history_str += "\n"
+		
+		# Summarize final actions and round outcomes
+		if i+1 == len(round_):
+			your_payoff = round_['outcome'][-1][f'{player_a}_payoff'] if current_player == player_a else round_['outcome'][-1][f'{player_b}_payoff']
+			your_final_action = round_['message_round'][-1]['action'] if round_['message_round'][-1]['player'] == current_player else round_['message_round'][-2]['action']
+
+			other_payoff = round_['outcome'][-1][f'{player_a}_payoff'] if current_player != player_a else round_['outcome'][-1][f'{player_b}_payoff']
+			other_final_action = round_['message_round'][-1]['action'] if round_['message_round'][-1]['player'] != current_player else round_['message_round'][-2]['action']
+   
+			history_str += f"Outcome of round {i+1}: At the end of the round, you chose {your_final_action}. On the other hand, the other party chose {other_final_action}. This resulted in a payoff of {your_payoff} for you and a payoff of {other_payoff} for the other party.\n"
+		
+		history_str += "\n"
+	
+	# Summarize the new round so far, if there is info
+	if round_outputs:
+		history_str += f"Current round:\n"
+		for j, message_round in round_outputs:
+			player = message_round[0]
+			reasoning = message_round[1]['reasoning']
+			message = message_round[1]['message']
+			action = message_round[1]['action']
+			if show_past_reasoning and (player == current_player):
+				history_str += f"Your reasoning: {reasoning}\n"
+			
+			if messages and (player == current_player):
+				history_str += f"Your message: {message}\n"
+
+			if messages and (player != current_player):
+				history_str += f"Other party's message: {message}\n"
+
+			if show_intended_actions and (player == current_player):
+				history_str += f"Your intended action at t={j+1} within this round: {action}\n"
+	
+	return history_str.strip()
+
 def run_round(policy_dict, names_tuple, matrix, labels_row, labels_column, results_dict, end_prob, total_message_rounds):
 	# Randomly choose who speaks first, then create tuple of speaker indices that alternate
 	first_speaker_index = np.random.choice(2)
 	speaker_indices = (lambda n, x: tuple(x if i % 2 == 0 else 1 - x for i in range(n)))(total_message_rounds, first_speaker_index)	
 	
 	# Conduct message exchange and get final action from player who sends the final message
-	outputs = []
+	round_outputs = []
 
 	for ix, speaker_index in enumerate(speaker_indices):
 		speaker, non_speaker = names_tuple[speaker_index], names_tuple[1-speaker_index]
 		policy = policy_dict[speaker]
-		history_str = results_to_history_str(policy, names_tuple, results_dict,  current_round_info)
+		history_str = results_to_history_str(policy, names_tuple, results_dict, round_outputs)
 		output = policy.query_policy(history_str, ix+1, labels_row, labels_column, end_prob, matrix, total_message_rounds) # LLM thinks and speaks here
-		outputs.append((speaker, output))
+		round_outputs.append((speaker, output))
 		
 	# In the case where no messages are exchanged, both players only select actions (to avoid messages, message_round is made > total_message_rounds).
 	if total_message_rounds == 0:
 		speaker_index = first_speaker_index
 		speaker, non_speaker = names_tuple[speaker_index], names_tuple[1-speaker_index]
 		policy = policy_dict[speaker]
-		history_str = results_to_history_str(policy, names_tuple, results_dict, current_round_info)
+		history_str = results_to_history_str(policy, names_tuple, results_dict, round_outputs)
 		output = policy.query_policy(history_str, total_message_rounds+1, labels_row, labels_column, end_prob, matrix, total_message_rounds) # LLM thinks and speaks here
-		outputs.append((speaker, output))
-
+		round_outputs.append((speaker, output))
  
 	# Get final action from the player who didn't get to send the final message (if there are message rounds) and the second player (if there are no message rounds)
 	last_index = 1 - speaker_index
 	speaker, non_speaker = names_tuple[last_index], names_tuple[1-last_index]
 	policy = policy_dict[speaker]
-	history_str = results_to_history_str(policy, names_tuple, results_dict, current_round_info)
+	history_str = results_to_history_str(policy, names_tuple, results_dict, round_outputs)
 	output = policy.query_policy(history_str, total_message_rounds + 1, labels_row, labels_column, end_prob, matrix, total_message_rounds)
-	outputs.append((speaker, output))
+	round_outputs.append((speaker, output))
 
-	return outputs
+	return round_outputs
 
 def update_results(results_dict, round_outputs, i, policy_dict, payoff_dict, names_tuple):
 	"""
@@ -311,20 +288,29 @@ def update_results(results_dict, round_outputs, i, policy_dict, payoff_dict, nam
 	results_dict['rounds'].append({})
 	results_dict['rounds'][-1]['message_rounds'] = []
 	results_dict['rounds'][-1]['outcome'] = {f'{player_a}_payoff': player_a_payoff, f'{player_b}_payoff': player_b_payoff}
-	for message_round, output in enumerate(round_outputs):
-		player = output[0]
-		reasoning = output[1]['reasoning']
-		message = output[1]['message']
-		action = output[1]['action']
+	for k, message_round in enumerate(round_outputs):
+		player = message_round[0]
+		reasoning = message_round[1]['reasoning']
+		message = message_round[1]['message']
+		action = message_round[1]['action']
 		results_dict['rounds'][-1]['message_rounds'].append({'player': player, 'reasoning': reasoning, 'message': message, 'action': action})
 
 	return results_dict
 
-def run_game(player_a_policy, player_b_policy, end_prob: float, fname: str, matrix: list, max_game_rounds: int =20, total_message_rounds: int =1):
-	# Make deep copies of policies
+def run_game(player_a_policy, player_b_policy, end_prob: float, fname: str, matrix: list[list], max_game_rounds: int = 20, total_message_rounds: int = 3):
+	# Check errors and if passed, make deep copies of policies
+	if (not isinstance(player_a_policy, Policy)) or (not isinstance(player_b_policy, Policy)):
+		raise TypeError('Argument is not an instance of the Policy class')
+	if player_a_policy.player_name == player_b_policy.player_name:
+		raise ValueError('Players cannot have the same name')
+	if fname == "":
+		raise ValueError('Filename cannot be empty')
+	if not (0 <= end_prob <= 1):
+		raise ValueError('Probability of termination out of range')
+
 	player_a_policy = deepcopy(player_a_policy)
 	player_b_policy = deepcopy(player_b_policy)
-	
+	 
 	# Initialize values and dictionaries to store results and values
 	results_dict = {'summary': {}, 'rounds': []}
 	results_dict['summary']['player_a_total_utility'] = 0.
@@ -377,20 +363,24 @@ def main():
 	player_a_name = "Alice"
 	player_a_role = "row"
 	player_a_persona = 'vanilla'
+	sys_prompt_substitutions_player_a = {}
 	player_a_strategic_reasoning = True
-	show_past_reasoning_player_a = False
+	show_past_reasoning_player_a = True
+	show_intended_actions_player_a = True
 	
 	# Player B Parameters
 	model_b = 'gpt-4'
 	player_b_name = "Bob"
 	player_b_role = "column"
 	player_b_persona = 'vanilla'
+	sys_prompt_substitutions_player_b = {}
 	player_b_strategic_reasoning = True
-	show_past_reasoning_player_b = False
+	show_past_reasoning_player_b = True
+	show_intended_actions_player_b = True
 	
 	# Initialize policies. Deepcopies are made in run_game()
-	player_a_policy = Policy(model_a, player_a_name, player_b_name, player_a_role, player_a_persona, player_a_strategic_reasoning, show_past_reasoning_player_a)
-	player_b_policy = Policy(model_b, player_b_name, player_a_name, player_b_role, player_b_persona, player_b_strategic_reasoning, show_past_reasoning_player_b)
+	player_a_policy = Policy(model_a, player_a_name, player_b_name, player_a_role, player_a_persona, player_a_strategic_reasoning, sys_prompt_substitutions_player_a, show_past_reasoning_player_a, show_intended_actions_player_a)
+	player_b_policy = Policy(model_b, player_b_name, player_a_name, player_b_role, player_b_persona, player_b_strategic_reasoning, sys_prompt_substitutions_player_b, show_past_reasoning_player_b, show_intended_actions_player_b)
 	
 	# Run games in parallel
 	max_workers = 4
